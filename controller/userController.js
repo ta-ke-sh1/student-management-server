e45fd21c37fe8094a7f3e5f4d3e9f83cca01a65f
takesh1 <85833065+ta-ke-sh1@users.noreply.github.com>
2023-09-23 18:06:13 +0700

    user api

--- controller/userController.js
+++ controller/userController.js
@@ -3,8 +3,13 @@ const { fetchAllData } = require("../repository/firebaseRepository");
 const constants = require("../utils/constants");
 const router = express.Router();
 
+const AES = require("crypto-js/aes");
+
 const multer = require("multer");
 const fs = require("fs");
+const { UserService } = require("../services/userService");
+
+const userService = new UserService();
 
 const uploader = multer({
     storage: multer.diskStorage({
@@ -26,10 +31,27 @@ router.get("/", async (req, res) => {
     res.status(200).send(users);
 });
 
-router.post("/", (req, res) => {});
+router.post("/", async (req, res) => {
+    let user = {
+        ...req.body,
+        avatar: "/avatar/default_avatar.jpg",
+        status: "activated",
+        password: AES.encrypt("123456", process.env.ENCRYPT_KEY).toString(),
+    };
+
+    console.log(user);
+
+    let response = await userService.addUser(user);
+    res.status(200).json({ result: response.msg });
+});
 
 router.delete("/", (req, res) => {});
 
-router.put("/", (req, res) => {});
+router.put("/", async (req, res) => {
+    const id = req.query.id;
+    const update_obj = req.body;
+    let response = await userService.editUser(id, update_obj);
+    res.status(200).json({ result: response.msg });
+});
 
 module.exports = router;
