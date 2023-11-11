92aef35d3bc52366a151618460449308e2335ad6
takesh1 <85833065+ta-ke-sh1@users.noreply.github.com>
2023-11-11 11:58:44 +0700

    delete submission

--- controller/submissionController.js
+++ controller/submissionController.js
@@ -14,108 +14,109 @@ const fileService = new FileService();
 const gradingService = new GradingService();
 
 router.get("/", async (req, res) => {
-  try {
-    const id = req.query.id;
-    console.log(req.query);
-    let result = await courseService.fetchSubmissionById(id);
-    res.status(200).json({
-      status: true,
-      data: result,
-    });
-  } catch (e) {
-    res.status(200).json({
-      status: false,
-      data: e.toString(),
-    });
-  }
+    try {
+        const id = req.query.id;
+        console.log(req.query);
+        let result = await courseService.fetchSubmissionById(id);
+        res.status(200).json({
+            status: true,
+            data: result,
+        });
+    } catch (e) {
+        res.status(200).json({
+            status: false,
+            data: e.toString(),
+        });
+    }
 });
 
 router.post("/", uploader.array("files", 10), async (req, res) => {
-  try {
-    let data = await gradingService.addGrading(req.body, req.files);
-    res.status(200).json({
-      status: true,
-      data: data,
-    });
-  } catch (e) {
-    res.status(200).json({
-      status: false,
-      data: e.toString(),
-    });
-  }
+    try {
+        let data = await gradingService.addGrading(req.body, req.files);
+        res.status(200).json({
+            status: true,
+            data: data,
+        });
+    } catch (e) {
+        res.status(200).json({
+            status: false,
+            data: e.toString(),
+        });
+    }
 });
 
 router.put("/", uploader.array("file", 10), async (req, res) => {
-  try {
-    fileService.removeFileByPath(req.query.path);
-    res.status(200).json({
-      status: true,
-    });
-  } catch (e) {
-    res.status(200).json({
-      status: false,
-      data: e.toString(),
-    });
-  }
+    try {
+        fileService.removeFileByPath(req.query.path);
+        res.status(200).json({
+            status: true,
+        });
+    } catch (e) {
+        res.status(200).json({
+            status: false,
+            data: e.toString(),
+        });
+    }
 });
 
-router.delete("/", async (req, res) => {
-  try {
-    fileService.removeFileByPath(req.query.path);
-    res.status(200).json({
-      status: true,
-    });
-  } catch (e) {
-    res.status(200).json({
-      status: false,
-      data: e.toString(),
-    });
-  }
+router.delete("/file", async (req, res) => {
+    try {
+        console.log(req.query);
+        fileService.removeFileByPath(req.query.path);
+        res.status(200).json({
+            status: true,
+        });
+    } catch (e) {
+        res.status(200).json({
+            status: false,
+            data: e.toString(),
+        });
+    }
 });
 
 router.post("/submit", uploader.array("files", 10), async (req, res) => {
-  try {
-    let result = await gradingService.addGrading(req.body, req.files);
+    try {
+        let result = await gradingService.addGrading(req.body, req.files);
 
-    res.status(200).send({
-      status: true,
-      data: result,
-    });
-  } catch (e) {
-    res.status(200).send({
-      status: false,
-      data: e.toString(),
-    });
-  }
+        res.status(200).send({
+            status: true,
+            data: result,
+        });
+    } catch (e) {
+        res.status(200).send({
+            status: false,
+            data: e.toString(),
+        });
+    }
 });
 
 router.put("/submit", uploader.array("items", 10), async (req, res) => {
-  try {
-    var fileNames = [];
-    console.log(req.files)
-    for (let i = 0; i < req.files.length; i++) {
-      let filename = req.files[i].filename;
-      let timestamp = parseInt(filename.split("&")[0]);
-      fileNames.push({ name: filename, timestamp: timestamp });
+    try {
+        var fileNames = [];
+        console.log(req.files);
+        for (let i = 0; i < req.files.length; i++) {
+            let filename = req.files[i].filename;
+            let timestamp = parseInt(filename.split("&")[0]);
+            fileNames.push({ name: filename, timestamp: timestamp });
+        }
+
+        let assignment = req.body;
+        delete assignment.fileNames;
+        assignment.submissions = fileNames;
+
+        let result = "";
+        // let result = await courseService.submitAssignment(assignment);
+
+        res.status(200).send({
+            status: true,
+            data: result,
+        });
+    } catch (e) {
+        res.status(200).send({
+            status: false,
+            data: e.toString(),
+        });
     }
-
-    let assignment = req.body;
-    delete assignment.fileNames;
-    assignment.submissions = fileNames;
-
-    let result = "";
-    // let result = await courseService.submitAssignment(assignment);
-
-    res.status(200).send({
-      status: true,
-      data: result,
-    });
-  } catch (e) {
-    res.status(200).send({
-      status: false,
-      data: e.toString(),
-    });
-  }
 });
 
 module.exports = router;
--- services/gradingService.js
+++ services/gradingService.js
@@ -1,92 +1,132 @@
-const { fetchDataById, addData, deleteData, updateData, fetchMatchingDataByField, setData } = require("../repository/firebaseRepository");
+const {
+    fetchDataById,
+    addData,
+    deleteData,
+    updateData,
+    fetchMatchingDataByField,
+    setData,
+} = require("../repository/firebaseRepository");
 const GradeRepository = require("../repository/gradeRepository");
 const constants = require("../utils/constants");
 const path = require("path");
 const FileService = require("./fileService");
 
 const GradingService = class {
-  gradeRepository;
-  fileService;
+    gradeRepository;
+    fileService;
 
-  constructor() {
-    this.gradeRepository = new GradeRepository();
-    this.fileService = new FileService();
-  }
-
-  async validGrading() { }
-
-  async reserveGrading() { }
-
-  async fetchAllGrades() {
-    return await this.gradeRepository.fetchAllGrades();
-  }
-
-  async fetchAllGradesByStudentId(id) {
-    return await this.gradeRepository.fetchAllGradesByStudentId(id);
-  }
-
-  async fetchAllGradesByStudentIdAndSemester(query) {
-    if (!query.id || !query.semester) throw "Missing parameter!";
-    return await this.gradeRepository.fetchAllGradesByStudentId(query.id);
-  }
-
-  async fetchAllGradesByStudentIdAndTermAndProgrammeAndDepartmentAndGroup(query) {
-    if (!query.id || !query.term || !query.programme || !query.department || !query.group) throw "Missing parameter!";
-    return await this.gradeRepository.fetchAllGradesByStudentIdAndTermAndProgrammeAndDepartmentAndGroup(query.id, query.term, query.programme, query.department, query.group);
-  }
-
-  async editGrading(grading_id, grading_obj) {
-    const res = await updateData(constants.SUBMISSIONS_TABLE, grading_id, grading_obj);
-    return res;
-  }
-
-  async deleteGrading(grading_id) {
-    const res = await updateData(constants.SUBMISSIONS_TABLE, grading_id, {
-      status: false,
-    });
-    return res;
-  }
-
-  async deleteGradingHard(grading_id) {
-    const res = await deleteData(constants.SUBMISSIONS_TABLE, grading_id);
-    return res;
-  }
-
-  async addGrading(grading_obj, files) {
-    const p = "\\submissions\\"
-      + grading_obj.programme
-      + "\\" + grading_obj.course_id
-      + "\\" + grading_obj.assignment_id
-      + "\\" + grading_obj.user_id
-      + "\\";
-    const folder = path.resolve() + "\\asset" + p;
-    this.fileService.addMultipleFilesByPath(files, folder);
-
-    let fileNames = [];
-    files.forEach((file) => {
-      fileNames.push(file.originalname);
-    })
-
-    delete grading_obj.files;
-    grading_obj.path = p;
-    grading_obj.grade = 0;
-    grading_obj.date = new Date().getTime();
-    grading_obj.fileNames = fileNames
-
-    const id = grading_obj.course_id + "-" + grading_obj.assignment_id + "-" + grading_obj.user_id
-    return await setData(constants.SUBMISSIONS_TABLE, id, grading_obj);
-  }
-
-  async fetchGradingById(Grading_id) {
-    if (!Grading_id) {
-      throw "Invalid Grading id!";
+    constructor() {
+        this.gradeRepository = new GradeRepository();
+        this.fileService = new FileService();
     }
-    let grading = await fetchDataById(constants.SUBMISSIONS_TABLE, Grading_id);
-    if (grading === -1) {
-      throw "Grading does not exist";
+
+    async validGrading() {}
+
+    async reserveGrading() {}
+
+    async fetchAllGrades() {
+        return await this.gradeRepository.fetchAllGrades();
     }
-    return grading;
-  }
+
+    async fetchAllGradesByStudentId(id) {
+        return await this.gradeRepository.fetchAllGradesByStudentId(id);
+    }
+
+    async fetchAllGradesByStudentIdAndSemester(query) {
+        if (!query.id || !query.semester) throw "Missing parameter!";
+        return await this.gradeRepository.fetchAllGradesByStudentId(query.id);
+    }
+
+    async fetchAllGradesByStudentIdAndTermAndProgrammeAndDepartmentAndGroup(
+        query
+    ) {
+        if (
+            !query.id ||
+            !query.term ||
+            !query.programme ||
+            !query.department ||
+            !query.group
+        )
+            throw "Missing parameter!";
+        return await this.gradeRepository.fetchAllGradesByStudentIdAndTermAndProgrammeAndDepartmentAndGroup(
+            query.id,
+            query.term,
+            query.programme,
+            query.department,
+            query.group
+        );
+    }
+
+    async editGrading(grading_id, grading_obj) {
+        const res = await updateData(
+            constants.SUBMISSIONS_TABLE,
+            grading_id,
+            grading_obj
+        );
+        return res;
+    }
+
+    async deleteGrading(grading_id) {
+        const res = await updateData(constants.SUBMISSIONS_TABLE, grading_id, {
+            status: false,
+        });
+        return res;
+    }
+
+    async deleteGradingHard(grading_id) {
+        const res = await deleteData(constants.SUBMISSIONS_TABLE, grading_id);
+        return res;
+    }
+
+    async addGrading(grading_obj, files) {
+        const p =
+            "\\submissions\\" +
+            grading_obj.programme +
+            "\\" +
+            grading_obj.course_id +
+            "\\" +
+            grading_obj.assignment_id +
+            "\\" +
+            grading_obj.user_id +
+            "\\";
+        const folder = path.resolve() + "\\asset" + p;
+        this.fileService.removeFileByPath(folder);
+
+        this.fileService.addMultipleFilesByPath(files, folder);
+
+        let fileNames = [];
+        files.forEach((file) => {
+            fileNames.push(file.originalname);
+        });
+
+        delete grading_obj.files;
+        grading_obj.path = p;
+        grading_obj.grade = 0;
+        grading_obj.date = new Date().getTime();
+        grading_obj.fileNames = fileNames;
+
+        const id =
+            grading_obj.course_id +
+            "-" +
+            grading_obj.assignment_id +
+            "-" +
+            grading_obj.user_id;
+        return await setData(constants.SUBMISSIONS_TABLE, id, grading_obj);
+    }
+
+    async fetchGradingById(Grading_id) {
+        if (!Grading_id) {
+            throw "Invalid Grading id!";
+        }
+        let grading = await fetchDataById(
+            constants.SUBMISSIONS_TABLE,
+            Grading_id
+        );
+        if (grading === -1) {
+            throw "Grading does not exist";
+        }
+        return grading;
+    }
 };
 
 module.exports = { GradingService };
